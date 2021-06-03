5e6e9f01c8916b69e4a9caf345d5a98399bb782c
Rodrigo Dionizio <rodrigosantiagodionizio@hotmail.com>
2021-06-03 17:54:06 -0300

    Utilizando Binding na atribuição de referência

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import "./estilo.css"
 
 class FormularioCadastro extends Component {
+
+    constructor(){
+        super();
+        this.titulo="";
+    }
+
+    handleMudançaTitulo(evento){
+        this.titulo = evento.target.value;
+    }
+
     render() {
         return (
             <form className="form-cadastro">
-                <input type="text" placeholder="Título" className="form-cadastro__input" />
-                <textarea rows={15} cols={50} placeholder="Escreva sua nota..." className="form-cadastro__input"/>
+                <input
+                    type="text"
+                    placeholder="Título"
+                    className="form-cadastro__input"
+                    onChange={this.handleMudançaTitulo.bind(this)}
+                />
+                <textarea rows={15} cols={50} placeholder="Escreva sua nota..." className="form-cadastro__input" />
                 <button className="form-cadastro__input form-cadastro__submit"> Criar Nota </button>
             </form>
         );
