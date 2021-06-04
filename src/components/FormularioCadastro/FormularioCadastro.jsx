5a1f15cb8abfbde226db4917eb230e1d06e3f947
Rodrigo Dionizio <rodrigosantiagodionizio@hotmail.com>
2021-06-04 00:39:41 -0300

    Novas Função call to action

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -6,15 +6,26 @@ class FormularioCadastro extends Component {
     constructor() {
         super();
         this.titulo = "";
+        this.texto = "";
     }
 
     handleMudançaTitulo(evento) {
         this.titulo = evento.target.value;
     }
 
+    handleMudancaTexto(evento){
+        this.titulo = evento.target.value;
+    }
+
+    criarNota(evento){
+        alert(`uma nova nota foi criada {this.titulo} {this.texto}`)
+    }
+
     render() {
         return (
-            <form className="form-cadastro">
+            <form className="form-cadastro"
+                onSubmit={this.criarNota.bind(this)}
+            >
                 <input
                     type="text"
                     placeholder="Título"
@@ -26,6 +37,7 @@ class FormularioCadastro extends Component {
                     cols={50}
                     placeholder="Escreva sua nota..."
                     className="form-cadastro__input"
+                    onChange={this.handleMudancaTexto.bind(this)}
                 />
                 <button className="form-cadastro__input form-cadastro__submit">
                     Criar Nota
